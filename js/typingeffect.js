9cc250feed127afaaab7ae1f5a47eee174e132a6
Vishal Dhatrika <dvishal35@gmail.com>
2023-01-12 13:56:47 -0600

    new homepage for v2

--- js/typingeffect.js
+++ js/typingeffect.js
@@ -1,12 +1,19 @@
 
 var i = 0;
-var txt = document.getElementById('TypingEffectText').innerHTML;
-document.getElementById('TypingEffectText').innerHTML = '';
+var txt = document.getElementById('TypingEffectText').value;
+var sp = " ";
+document.getElementById('TypingEffectText').value = "";
+document.getElementById("TypingEffectText").style.border = "none";
+document.getElementById("TypingEffectText").style.outline = "none";
+document.getElementById("TypingEffectText").style.textAlign = "center";
+document.getElementById("TypingEffectText").style.resize = "none";
+
+//sp.repeat(txt.length);
 
 function typeWriter() {
-    var speed = 120;
+    var speed = 95;
     if (i < txt.length) {
-        document.getElementById("TypingEffectText").innerHTML += txt.charAt(i);
+        document.getElementById("TypingEffectText").value += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
